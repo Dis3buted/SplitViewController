66d26d7f05d8b0c730d163b4bc9e68bbab56d522
Dis3buted <Dis3buted@gmail.com>
2017-10-10 13:51:41 +0300

    Initial Commit

---
+++ SplitViewController/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SplitViewController
+//
+//  Created by Distributed on 10/10/2017.
+//  Copyright © 2017 Seven Years Later. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SplitViewController/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SplitViewController
+//
+//  Created by Distributed on 10/10/2017.
+//  Copyright © 2017 Seven Years Later. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
