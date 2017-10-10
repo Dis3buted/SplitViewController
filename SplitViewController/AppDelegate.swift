f407e9356150f68f71bed398e9ddb6ecf0e501d4
Dis3buted <Dis3buted@gmail.com>
2017-10-10 13:59:58 +0300

    Initial

--- SplitViewController/AppDelegate.swift
+++ SplitViewController/AppDelegate.swift
@@ -1,17 +1,8 @@
-//
-//  AppDelegate.swift
-//  SplitViewController
-//
-//  Created by Distributed on 10/10/2017.
-//  Copyright © 2017 Seven Years Later. All rights reserved.
-//
 
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -21,6 +12,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 }
 
